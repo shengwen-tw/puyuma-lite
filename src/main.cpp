9458029878718995301b91ced01dc8d2d2e49d8d
Shengwen Cheng <shengwen1997.tw@gmail.com>
2019-07-02 01:08:04 +0800

    Add main controller loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,42 @@
 #include <opencv2/opencv.hpp>
 
 #include "lane_detector.hpp"
+#include "self_driving.hpp"
 
 using namespace cv;
 
+cv::Mat camera_matrix, distort_coffecient;
+
 int main()
 {
-	cv::Mat raw_image;
 	float d, phi;
 
 	lane_estimator_init();
 
-	lane_estimate(raw_image, d, phi);
+	//self-driving system main loop
+	while(1) {
+		//TODO: retrieve image from pi camera
+		cv::Mat frame;
+		cv::Mat distort_image;
+
+		//calibrate the fish-eye camera image before doing lane detection
+		cv::undistort(frame, distort_image, camera_matrix, distort_coffecient);
+
+		/* lane estimation */
+		float d = 0, phi = 0;
+		bool get_pose = lane_estimate(distort_image, d, phi);
+
+                if(get_pose == true) {
+                        self_driving_control(d, phi);
+                } else {
+                        //halt_motor();
+                }
+	}
 
 	return 0;
+}
+
+__attribute__((destructor))void end()
+{
+	//halt_motor();
 }
